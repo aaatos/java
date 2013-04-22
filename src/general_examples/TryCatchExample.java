8bcffb3a487ae987dead7e7b7cd551d043c6ee6d
angelos <angelos@angelos-System-Product-Name>
2013-04-22 15:03:02 +0300

    30-04/2012 15:30

--- src/general_examples/TryCatchExample.java
+++ src/general_examples/TryCatchExample.java
@@ -11,20 +11,38 @@ import java.util.Scanner;
  * @author angelos
  */
 public class TryCatchExample {
-    
-    int div (int x, int y){
-        return x/y;
-    }
-    
-    public static void main(String[] args) {
-        TryCatchExample object=new TryCatchExample();
-        Scanner input=new Scanner(System.in);
-        System.out.print("Δώσε τον πρώτο αριθμό ");
-        int a=input.nextInt();
+
+    String div() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Δώσε τον πρώτο αριθμό (99999 για έξοδο) ");
+        int a = input.nextInt();
+        if (a == 99999) {
+            return "exit";
+        }
         System.out.print("Δώσε τον δεύτερο αριθμό ");
-        int b=input.nextInt();
-        System.out.print("To άθροισμα είναι: ");
-        System.out.println(object.div(a,b));
-        
+        int b = input.nextInt();
+        return "To άθροισμα είναι: " + (a / b);
+
+    }
+
+    public static void main(String[] args) {
+        String result="";
+        boolean foundException = false;
+        TryCatchExample object = new TryCatchExample();
+        while ((!result.equals("exit")) && (foundException != true)) {
+            try {
+                result = object.div();
+                if (result.equals("exit")) {
+                    System.exit(0);
+                }
+            }
+            catch (ArithmeticException ex) {
+                System.out.println("ΠΡΟΣΟΧΗ!! Διαίρεση με το μηδέν.");
+                
+                        }
+        }
+
+
     }
 }
+
